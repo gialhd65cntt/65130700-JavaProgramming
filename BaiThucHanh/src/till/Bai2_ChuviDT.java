8ca82b65e02af963320e716f5383bd1cc3d4260b
Teddy <danggia22112005@gmail.com>
2026-02-01 17:15:56 +0700

    Create Bai2_ChuviDT.java

---
+++ BaiThucHanh/src/till/Bai2_ChuviDT.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package till;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class Bai2_ChuviDT {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Nhập chiều dài:");
+		int chieuDai= scanner.nextInt();
+		System.out.print("Nhập chiều rộng:");
+		int chieuRong= scanner.nextInt();
+		int chuVi=(chieuDai+chieuRong)*2;
+		int dienTich=chieuDai*chieuRong;
+		System.out.printf("Chu vi là: %d, Diện tích là: %d, cạnh nhỏ là: %d",chuVi,dienTich,chieuRong);
+	}
+
+}
